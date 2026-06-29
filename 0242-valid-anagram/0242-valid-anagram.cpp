6e292b1b3c754901de418833a5ea870c142692b1
Heet Jain <heetjainrr@gmail.com>
2026-06-30 02:44:46 +0530

    Time: 8 ms (7.08%), Space: 9.7 MB (76.01%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> mps, mpt;
+        for (auto word : s){
+            mps[word]++;
+        }
+        for (auto word : t){
+            mpt[word]++;
+        }
+        for (auto word : s){
+            if(mps[word] != mpt[word])
+                return false;
+        }
+        for (auto word : t){
+            if(mps[word] != mpt[word])
+                return false;
+        }
+        return true;
+    }
+};
+
+
